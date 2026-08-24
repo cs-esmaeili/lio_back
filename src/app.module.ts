45c915114087acb61dcde1f656d254e3e7249c4f
Javad Esmaeili <cs.esmaeili@gmail.com>
2026-08-24 21:41:19 +0330

    clean up project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
-import { UserService } from './user.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UserService],
+  providers: [PrismaService],
 })
 export class AppModule {}

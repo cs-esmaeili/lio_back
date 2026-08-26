b2c45fd13f04e0fac1c6ca936e0971fabdcf50af
Javad Esmaeili <cs.esmaeili@gmail.com>
2026-08-26 23:22:55 +0330

    basic password auth added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 @Controller()
 export class AppController {
-
   @Get()
   getHello(): string {
     return 'App is running !';
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/logout')
+  async logout(@Request() req) {
+    return req.logout();
   }
 }

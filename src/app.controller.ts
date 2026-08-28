43c471c68b560b0642f78377ec54a6e772b4edb5
Javad Esmaeili <cs.esmaeili@gmail.com>
2026-08-28 13:14:08 +0330

    refactor(app): remove scaffold auth routes conflicting with AuthController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,9 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
-import { AuthService } from './auth/services/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
-
   @Get()
   getHello(): string {
     return 'App is running !';
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/logout')
-  async logout(@Request() req) {
-    return req.logout();
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 }
